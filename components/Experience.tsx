69a6ec3af8bc9e8e95e068bd47d96596fab4e948
Aifia Mohamed Amine <aifiamohamedamine@isamm.u-manouba.tn>
2024-09-12 09:37:07 +0100

    experience_section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,10 +1,39 @@
+import * as React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import Typography from "@mui/material/Typography";
+import { Briefcase } from "lucide-react";
+
 const Experience = () => {
   return (
-    <section className="xl:ml-12 ml-2 xl:h-[600px] pb-12 xl:pb-24 ">
+    <section className="xl:ml-12 ml-2 xl:h-[600px] pb-12 xl:pb-24">
       <div className="container mx-auto">
         <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
           Experience
         </h2>
+
+        <Timeline position="alternate">
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineConnector />
+              <TimelineDot color="primary" variant="outlined">
+                <Briefcase />
+              </TimelineDot>
+              <TimelineConnector sx={{ bgcolor: "secondary.main" }} />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: "12px", px: 2 }}>
+              <Typography variant="h6" component="span">
+                Sleep
+              </Typography>
+              <Typography>Because you need rest</Typography>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
       </div>
     </section>
   );
